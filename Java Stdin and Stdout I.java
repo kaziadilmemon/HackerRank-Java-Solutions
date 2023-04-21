a688cbe7ed86f2a57c4906c6ee0620781db55284
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-21 19:56:51 +0500

    Create Java Stdin and Stdout I.java

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,30 @@
+/* In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line. 
+To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format: There are  lines of input, and each line contains a single integer.
+Sample Input
+
+42
+100
+125 */
+
+// Code: 
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+         int b = scan.nextInt();
+           int c = scan.nextInt();
+
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+}
+}
+
