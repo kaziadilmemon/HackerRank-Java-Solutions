f7bb308af1f2c62ead20a04b90a418d47de26bd0
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-23 22:14:12 +0500

    Create Java Datatypes.java

---
+++ Java Datatypes.java
@@ -0,0 +1,15 @@
+/* Task:
+Input Format
+
+The first line contains an integer,T, denoting the number of test cases.
+Each test case,T, is comprised of a single line with an integer, , which can be arbitrarily large or small.
+
+Output Format
+
+For each input variable n and appropriate primitive dataType , you must determine if the given primitives are capable of storing it. If yes, then print:
+
+n can be fitted in:
+* dataType
+If there is more than one appropriate data type, print each one on its own line and order them by size (i.e.: byte < short < int < long ).
+
+If the number cannot be stored in one of the four aforementioned primitives, print the line:
