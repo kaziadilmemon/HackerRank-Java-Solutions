d58276a48f097564f94a9c5b6b0aab726e072e3c
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-23 22:02:42 +0500

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,7 @@
+/* Objective
+In this challenge, we're going to use loops to help us do some simple math.
+
+Task:
+Given an integer, N , print its first 10  multiples. Each multiple N*i  
+(where 1<=i<=10 ) should be printed on a new line in the form: N x i = result. in java. */
+
