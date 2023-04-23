3b67a01a78418e69b9ce7f5fb0a1b492fc566641
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-23 17:22:50 +0500

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,28 @@
+
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+
+        String ans = "";
+
+        if (n % 2 == 1) {
+            ans = "Weird";
+        } else {
+            if (n >= 2 && n <= 5) {
+                ans = "Not Weird";
+            } else if (n >= 6 && n <= 20) {
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
