c346a8e39d7f1f89399ae1e33808b71ee937944a
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-23 22:03:56 +0500

    Update Java Loops I.java

--- Java Loops I.java
+++ Java Loops I.java
@@ -5,5 +5,20 @@ Task:
 Given an integer, N , print its first 10  multiples. Each multiple N*i  
 (where 1<=i<=10 ) should be printed on a new line in the form: N x i = result. in java. */
 
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Read input integer N
+        int N = scanner.nextInt();
+
+        // Print the first 10 multiples of N
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+
+        scanner.close();
+    }
+}
