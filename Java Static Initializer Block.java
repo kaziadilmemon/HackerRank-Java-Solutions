da77c762ffe9ed048e0e955887a64b3232a48e3a
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-28 22:04:45 +0500

    Update Java Static Initializer Block.java

--- Java Static Initializer Block.java
+++ Java Static Initializer Block.java
@@ -15,3 +15,38 @@ Output Format
 
 If both values are greater than zero, then the main method must output the area of the parallelogram. 
 Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+      static int B;
+    static int H;
+    static boolean flag;
+
+static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        scanner.close();
+
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            flag = false;
+        } else {
+            flag = true;
+        }
+    }
+    
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
