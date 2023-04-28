3b6ec6129acd8dac435d26b8daeffe1596a998bd
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-28 22:04:13 +0500

    Create Java Static Initializer Block.java

---
+++ Java Static Initializer Block.java
@@ -0,0 +1,17 @@
+/* 
+Question: 
+You are given a class Solution with a main method. Complete the given code so that it outputs the area of a parallelogram with breadth B and height H .
+You should read the variables from the standard input.
+
+If  B<=0 or H<=0 , the output should be "java.lang.Exception: Breadth and height must be positive" without  quotes.
+
+Input Format
+
+There are two lines of input. The first line contains B : the breadth of the parallelogram. The next line contains H: the height of the parallelogram.
+
+Constraints 
+-100<=B<=100 , -100<=H<=100
+Output Format
+
+If both values are greater than zero, then the main method must output the area of the parallelogram. 
+Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.*/
